4132af4d43d3414fb7dce250ea69ea01847693e2
Antelope <webantelopestudio@gmail.com>
2019-10-16 23:00:12 +0300

    Upgrade gulpfile.js

--- clear-html/gulpfile.js
+++ clear-html/gulpfile.js
@@ -1,5 +1,36 @@
-function defaultTask(cb) {
-  cb();
-}
+const
+  GULP = require('gulp'),
+  SASS = require('gulp-sass'),
+  BROWSERSYNC = require('browser-sync'),
+  RSYNC = require('gulp-rsync');
 
-exports.default = defaultTask;
+
+// WATCH
+GULP.task('watch', () => {
+  GULP.watch('app/*.html', GULP.parallel('code'));
+});
+
+// CODE & RELOAD
+GULP.task('rsync', () => {
+
+});
+
+
+// BROWSER-SYNC
+GULP.task('browser-sync', () => {
+  BROWSERSYNC({
+    server: {
+      baseDir: 'app',
+    },
+    notify: false,
+  })
+});
+
+GULP.task('default', GULP.parallel('browser-sync'));
+
+
+// function defaultTask(cb) {
+//   cb();
+// }
+//
+// exports.default = defaultTask;
